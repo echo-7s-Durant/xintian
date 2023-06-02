47d6b1d10337e0c11a8f09ff546196cfc86903d0
梁心田 <2223653445@qq.com>
2023-06-02 10:51:30 +0800

    first

---
+++ buildResult/.vuepress/.temp/internal/pagesComponents.js
@@ -0,0 +1,6 @@
+import { defineAsyncComponent } from 'vue'
+
+export const pagesComponents = {
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"C:/Users/田田/Desktop/博客/blog/buildResult/.vuepress/.temp/pages/404.html.vue")),
+}
